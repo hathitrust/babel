a418befb225352f1b59ec2b7929b2e3c16390261
Phillip Farber <pfarber@umich.edu>
2011-03-17 14:32:09 -0400

    * lib/Document.pm: add debugging when id not in j_rights

--- slip/lib/Document.pm
+++ slip/lib/Document.pm
@@ -166,6 +166,8 @@ sub ___get_rights_f_id {
     my $dbh = $db->get_DBH($C);
     my $attr = Db::Select_j_rights_id_attr($C, $dbh, $id);
 
+    DEBUG('doc', qq{METADATA: $id MISSING from j_rights}) if (! $attr);
+
     return $attr;
 }
 
