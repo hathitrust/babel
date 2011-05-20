24cd1f27230918a0ee0cacff28e033fc33c98f26
Roger Espinosa <roger@umich.edu>
2011-05-20 11:02:00 -0400

    Initial commit

---
+++ pt/web/js/classic_startup.js
@@ -0,0 +1,27 @@
+var HT = HT || {};
+
+HT.resizeBookReader = function() {
+  
+  var $container = $("#mdpContentContainer");
+  var $body = $("body");
+
+  if ( $.browser.msie && parseInt($.browser.version) < 8 ) {
+    var $window = $(window);
+    if ( $window.width() >= HT.config.ARBITRARY_WINDOW_WIDTH ) {
+      $body.width($window.width());
+    } else if ( $body.width() < HT.config.ARBITRARY_WINDOW_WIDTH ) {
+      $body.width(HT.config.ARBITRARY_WINDOW_WIDTH);
+    }
+  }
+
+  $container.width($body.width() - HT.config.ARBITRARY_PADDING);
+}
+
+
+$(window).bind("resize", function() {
+  HT.resizeBookReader();
+})
+
+$(document).ready(function() {
+  HT.resizeBookReader();
+})
