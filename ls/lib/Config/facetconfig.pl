fde3929889cb69d0e1b3f3fa9a30e0c54f91e910
tburtonw <tburtonw@umich.edu>
2012-01-20 12:06:22 -0500

    changed to two rows by changing $default_fields in facetconfig.pl

--- ls/lib/Config/facetconfig.pl
+++ ls/lib/Config/facetconfig.pl
@@ -144,9 +144,7 @@ $op_order=['AND','OR'];
 # default field for each row starting with fisrt row
 $default_fields = ['ocr',
                  'title',
-                 'author',
-                 'subject',
-                 ];
+                  ];
 # default any all phrase
 $default_anyall = ['all',
                    'all',
