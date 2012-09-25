60c2ace74bac2074a498e275c2b333f42ffd9623
Phillip Farber <pfarber@umich.edu>
2012-09-25 15:35:23 -0400

    get_institution --> get_institution_code
    checks both SDRINST and eduPersonScopedAffiliation and add new
    access_use banners

--- wayf/web/js/concatenated_common-min.js
+++ wayf/web/js/concatenated_common-min.js
@@ -1,4 +1,4 @@
 function goto_page(a){if(a!=""){if(a=="0"){alert("Please select your institution")}else{document.location.href=a}}return false};
-/* /htapps/roger.babel/wayf/web/js/login-min.js */
+/* /htapps/pfarber.babel/wayf/web/js/login-min.js */
 function loginButtonClick(){var a=document.getElementById("frontpage");a.style.display="none";a=document.getElementById("selectinst");a.style.display="";return false}function loginHomeClick(){var a=document.getElementById("selectinst");a.style.display="none";a=document.getElementById("frontpage");a.style.display="";return false};
-/* /htapps/roger.babel/wayf/web/js/loginMobile-min.js */
+/* /htapps/pfarber.babel/wayf/web/js/loginMobile-min.js */
