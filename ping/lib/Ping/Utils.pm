3b2562d2ed0fe2bc81ef8afdb7c3311b1da20ffe
Phillip Farber <pfarber@umich.edu>
2012-10-16 14:14:46 -0400

    Implement institution mapping

--- ping/lib/Ping/Utils.pm
+++ ping/lib/Ping/Utils.pm
@@ -1,48 +1,33 @@
 package Ping::Utils;
 
-sub expand_affiliation {
-    my ( $affiliation ) = @_;
-    # can we assume that institutions.xml is always going to be well formatted??
-    require File::Slurp;
-    my $institutions_filename = qq{$ENV{SDRROOT}/ping/web/common-web/institutions.xml};
-    my $match;
-    my $in_affiliation = 0;
-    foreach my $line ( File::Slurp::read_file($institutions_filename) ) {
-        if ( $line =~ m,"$affiliation", ) {
-            $in_affiliation = 1;
-        } elsif ( $line =~ m,enabled=, && $in_affiliation ) {
-            chomp $line;
-            $match = $line;
-            $match =~ s,^.*>([^<]+)<.*,$1,;
-            return $match;
-        }
-    }
-    return $affiliation;
-}
-    
+use Auth::Auth;  
+use Institutions;
+
 sub identify_user {
 
     my ( $C, $env ) = @_;
 
-    my $displayName = $$env{REMOTE_USER};
-    my $affiliation;
-    my $auth_type = lc $$env{AUTH_TYPE};
     my $auth = new Auth::Auth($C);
 
-    if ( $auth_type eq 'shibboleth' ) {
-        $displayName = $$env{displayName} || $auth->__get_prioritized_scoped_affiliation();
-        ## $affiliation = $auth->__get_prioritized_scoped_affiliation();
-        $affiliation = expand_affiliation($auth->get_institution_code($C));
-    } elsif ( $displayName !~ m,@,) {
-        $affiliation = 'University of Michigan';
-    }
-    
+    my $displayName = $auth->get_user_display_name($C, 'unscoped');
+    my $institution_code = $auth->get_institution_code($C);
+    my $institution_name = Institutions::get_institution_sdrinst_field_val($C, $institution_code, 'name', 'mapped');
     my $print_disabled = $auth->get_eduPersonEntitlement_print_disabled($C);
+
+    my $auth_type;
+    if ( $auth->auth_sys_is_SHIBBOLETH($C) ) {
+        $auth_type = 'shibboleth';
+    } 
+    elsif ( $auth->auth_sys_is_COSIGN($C) ) {
+        $auth_type = 'cosign';
+    } 
+
+
     
     return { authType => $auth_type, 
              displayName => $displayName, 
-             affiliation => $affiliation, 
-             institution => $auth->get_institution_code($C),
+             affiliation => $institution_name, 
+             institution => $institution_code,
              u => $print_disabled };
 
 }
