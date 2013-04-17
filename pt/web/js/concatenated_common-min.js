744a2278c9fd7085e2fa779ae8d67374bb95efc9
Roger Espinosa <roger@umich.edu>
2013-04-17 11:48:22 -0400

    more head.js

--- pt/web/js/concatenated_common-min.js
+++ pt/web/js/concatenated_common-min.js
@@ -432,109 +432,115 @@ if (!Date.now) {
   }
 }
 
-!function( $ ) {
-  
-  if (!Object.create) {
-      Object.create = function (o) {
-          if (arguments.length > 1) {
-              throw new Error('Object.create implementation only accepts the first parameter.');
+head.ready(function() {
+
+  !function( $ ) {
+    
+    if (!Object.create) {
+        Object.create = function (o) {
+            if (arguments.length > 1) {
+                throw new Error('Object.create implementation only accepts the first parameter.');
+            }
+            function F() {}
+            F.prototype = o;
+            return new F();
+        };
+    }
+    
+    // Create a plugin based on a defined object
+    $.plugin = function( name, object ) {
+      $.fn[name] = function( options ) {
+        return this.each(function() {
+          if ( ! $.data( this, name ) ) {
+            $.data( this, name, Object.create(object).init(
+            options, this ) );
+          } else {
+            var ob = $.data(this, name);
+            ob.handle(options);
           }
-          function F() {}
-          F.prototype = o;
-          return new F();
+        });
       };
-  }
-  
-  // Create a plugin based on a defined object
-  $.plugin = function( name, object ) {
-    $.fn[name] = function( options ) {
-      return this.each(function() {
-        if ( ! $.data( this, name ) ) {
-          $.data( this, name, Object.create(object).init(
-          options, this ) );
-        } else {
-          var ob = $.data(this, name);
-          ob.handle(options);
-        }
-      });
     };
-  };
-  
-}( window.jQuery );
+    
+  }( window.jQuery );
 
-/* jQuery Tiny Pub/Sub - v0.7 - 10/27/2011
- * http://benalman.com/
- * Copyright (c) 2011 "Cowboy" Ben Alman; Licensed MIT, GPL */
- 
-(function($) {
- 
-  // var o = $({});
-  var o = $(document);
- 
-  $.subscribe = function() {
-    o.on.apply(o, arguments);
-  };
- 
-  $.unsubscribe = function() {
-    o.off.apply(o, arguments);
-  };
- 
-  $.publish = function() {
-    o.trigger.apply(o, arguments);
-  };
+  /* jQuery Tiny Pub/Sub - v0.7 - 10/27/2011
+   * http://benalman.com/
+   * Copyright (c) 2011 "Cowboy" Ben Alman; Licensed MIT, GPL */
+   
+  (function($) {
+   
+    // var o = $({});
+    var o = $(document);
+   
+    $.subscribe = function() {
+      o.on.apply(o, arguments);
+    };
+   
+    $.unsubscribe = function() {
+      o.off.apply(o, arguments);
+    };
+   
+    $.publish = function() {
+      o.trigger.apply(o, arguments);
+    };
 
-  $.fn.subscribe = function(trigger, fn) {
-    var $objects = this;
-    $.subscribe(trigger, function() {
-      var args = arguments;
-      $objects.each(function() {
-        fn.apply(this, args);
-      })
-    });
-    return $objects;
-  }
- 
-}(jQuery));
-
-var $window;
-
-$(document).ready(function() {
-
-  $window = $(window);
-
-  // bind the resize for IE8
-  if ( $.browser.msie ) {
-    if ( parseInt($.browser.version) <= 8 ) {
-      $window = $("body");
-      console.log("REDEFINING $window");
+    $.fn.subscribe = function(trigger, fn) {
+      var $objects = this;
+      $.subscribe(trigger, function() {
+        var args = arguments;
+        $objects.each(function() {
+          fn.apply(this, args);
+        })
+      });
+      return $objects;
     }
-  }
+   
+  }(jQuery));
 
-  //** THESE COULD BE DONE IN reader.js
+  var $window;
 
-  // and bind the search form for validation
-  $("#form-search-volume").submit(function() {
-    var $form = $(this);
-    var $input = $form.find("input[type=text]")
-    if ( ! $.trim($input.val()) ) {
-      bootbox.alert("Please enter a term in the search box.");
-      return false;
+  $(document).ready(function() {
+
+    $window = $(window);
+
+    // bind the resize for IE8
+    if ( $.browser.msie ) {
+      if ( parseInt($.browser.version) <= 8 ) {
+        $window = $("body");
+        console.log("REDEFINING $window");
+      }
     }
-    return true;
-  })
 
-  // same with any existing page number
-  $("#form-go-page").submit(function() {
-    var $form = $(this);
-    var $input = $form.find("input[type=text]")
-    if ( ! $.trim($input.val()) ) {
-      bootbox.alert("Please enter a page number.");
-      return false;
-    }
-    return true;
+    //** THESE COULD BE DONE IN reader.js
+
+    // and bind the search form for validation
+    $("#form-search-volume").submit(function() {
+      var $form = $(this);
+      var $input = $form.find("input[type=text]")
+      if ( ! $.trim($input.val()) ) {
+        bootbox.alert("Please enter a term in the search box.");
+        return false;
+      }
+      return true;
+    })
+
+    // same with any existing page number
+    $("#form-go-page").submit(function() {
+      var $form = $(this);
+      var $input = $form.find("input[type=text]")
+      if ( ! $.trim($input.val()) ) {
+        bootbox.alert("Please enter a page number.");
+        return false;
+      }
+      return true;
+    })
+
   })
 
 })
+
+
 
 /* /htapps/roger.babel/pt/web/js/base.js */
 // supply method for feedback system
