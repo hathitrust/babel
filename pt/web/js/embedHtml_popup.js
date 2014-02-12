eaeae37e4a14a017eade045015ae22181391a0be
Andy Mardesich <andy.mardesich@ucop.edu>
2014-02-12 10:23:56 -0500

    HTP-305 Add embed HTML to Pageturner; Tightened Up
    Signed-off-by: Roger Espinosa <roger@umich.edu>

--- pt/web/js/embedHtml_popup.js
+++ pt/web/js/embedHtml_popup.js
@@ -4,28 +4,29 @@ head.ready(function() {
     var side_short = "450";
     var side_long  = "700";
     var htId = HT.params.id;
+    var embedHelpLink = "http://www.hathitrust.org/embed";
 
-    var codeblock_txt_a = function(w,h) {
-        return '<iframe width="' + w + '" height="' + h + '" '; 
-    }
-    var codeblock_txt_b = 'src="http://hdl.handle.net/2027/' + htId + 
-        '?urlappend=%3Bui=embed"></iframe>';
+    var codeblock_txt_a = function(w,h) {return '<iframe width="' + w + '" height="' + h + '" ';}
+    var codeblock_txt_b = 'src="http://hdl.handle.net/2027/' + htId + '?urlappend=%3Bui=embed"></iframe>';
 
     var $block = $(
 	'<div class="embedUrlContainer">' +
         '<h3>Embed This Book</h3>' +
-	'<a id="embedIcon" default-form="data-default-form" href="http://www.hathitrust.org/embed" target="_blank">Help</a>' +
+	'<a id="embedHelpIcon" default-form="data-default-form" ' +
+	  'href="' + embedHelpLink + '" target="_blank">Help</a>' +
         '<form>' + 
         '    <span class="help-block">Copy the code below and paste it into the HTML of any website or blog.</span>' +
         '    <label for="codeblock" class="offscreen">Code Block</label>' +
-        '    <textarea class="input-xlarge" id="codeblock" name="codeblock" rows="3" readonly="readonly">' +
+        '    <textarea class="input-xlarge" id="codeblock" name="codeblock" rows="3">' +
         codeblock_txt_a(side_short, side_long) + codeblock_txt_b + '</textarea>' + 
         '<div class="controls">' + 
             '<label class="radio inline">' +
-                '<input type="radio" name="view" id="edit-view-0" value="0" checked="checked" > <span class="icomoon-scroll"/> Scroll View ' +
+                '<input type="radio" name="view" id="view-scroll" value="0" checked="checked" >' +
+		'<span class="icomoon-scroll"/> Scroll View ' +
             '</label>' + 
             '<label class="radio inline">' +
-                '<input type="radio" name="view" id="edit-view-1" value="1" > <span class="icomoon-book-alt2"/> Flip View ' +
+                '<input type="radio" name="view" id="view-flip" value="1" >' +
+		'<span class="icomoon-book-alt2"/> Flip View ' +
             '</label>' +
         '</div>' +
         '</form>' +
@@ -40,18 +41,23 @@ head.ready(function() {
                 "label" : "Cancel",
                 "class" : "btn-dismiss"
             }
-        ]);
+	]);
 
+        // Custom width for bounding '.modal' 
+        $block.closest('.modal').addClass("bootboxMediumWidth");
+
+        // Select entirety of codeblock for easy copying
         var textarea = $block.find("textarea[name=codeblock]");
 	textarea.on("click", function () {
 	    $(this).select();
 	});
 
-        $('input:radio[id="edit-view-0"]').click(function () {
+        // Modify codeblock to one of two views 
+        $('input:radio[id="view-scroll"]').click(function () {
 	    codeblock_txt = codeblock_txt_a(side_short, side_long) + codeblock_txt_b; 
             textarea.val(codeblock_txt);
         });
-        $('input:radio[id="edit-view-1"]').click(function () {
+        $('input:radio[id="view-flip"]').click(function () {
 	    codeblock_txt = codeblock_txt_a(side_long, side_short) + codeblock_txt_b; 
             textarea.val(codeblock_txt);
         });
