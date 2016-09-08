689cc0bb9366fb44f176d3a5c6052893b2d32b22
tburtonw <tburtonw@umich.edu>
2016-09-08 14:24:00 -0400

    tracking submodules and fixing run-11.conf for language008

--- slip/lib/Sl_Utils.pm
+++ slip/lib/Sl_Utils.pm
@@ -22,11 +22,20 @@ Coding example
 use strict;
 use warnings;
 
+
 use Date::Calc qw(:all);
 
 use Context;
 use MdpConfig;
+use File::Path qw(make_path);
 
+use constant SLIP_TMP_DIR => "/tmp/slip_semaphores";
+
+
+# Create the tmp dir if it doesn't already exist.
+BEGIN {
+    make_path(SLIP_TMP_DIR);
+}
 
 # ---------------------------------------------------------------------
 
@@ -39,11 +48,45 @@ Avoid full optimization on checkIndex days, for example.
 # ---------------------------------------------------------------------
 sub checkIndex_day {
     my $C = shift;
-    
+
     my $day_of_week = Day_of_Week_to_Text(Day_of_Week(Today()));
     my $check_index_day = $C->get_object('MdpConfig')->get('check_index_day_of_week');
     return ($day_of_week eq $check_index_day);
 }
+
+
+#-------------------------------------------------
+=item slip_lockfile_dir
+
+Return the slip lockfile dir, as stored in the constant SLIP_TMP_DIR.
+We make sure to create the directory when this file is loaded
+just in case.
+
+=cut
+
+#-----------------------------------------------------
+
+sub slip_lockfile_dir {
+    return SLIP_TMP_DIR;
+}
+
+#-------------------------------------------------
+=item slip_semaphore_file(file_base)
+
+Return a filename based on the passed-in string and
+the slip_lockfile_dir, of the form
+<file_base>-lock.sem
+
+=cut
+
+#-----------------------------------------------------
+
+sub slip_semaphore_file {
+    my $file_base = shift;
+    my $filename = $file_base . '-lock.sem';
+    return join('/', SLIP_TMP_DIR, $filename);
+}
+
 
 1;
 
