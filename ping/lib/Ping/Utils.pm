bf7b10f50026e1cf1ad7d89a4811e6c6a182fe9d
Roger Espinosa <roger@umich.edu>
2017-09-27 14:46:42 -0400

    uppercase affiliation

--- ping/lib/Ping/Utils.pm
+++ ping/lib/Ping/Utils.pm
@@ -13,7 +13,7 @@ sub identify_user {
     my $displayName = $auth->get_user_display_name($C, 'unscoped');
     my $institution_code = $auth->get_institution_code($C, 'mapped');
     my $institution_name = $auth->get_institution_name($C, 'mapped');
-    my $affiliation = $auth->get_eduPersonUnScopedAffiliation($C);
+    my $affiliation = ucfirst($auth->get_eduPersonUnScopedAffiliation($C));
     my $print_disabled = $auth->get_eduPersonEntitlement_print_disabled($C);
 
     my $providerName = $auth->get_institution_name($C, 'mapped', 1);
