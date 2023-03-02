12511671eda82e96919c27f0f5cb8efd3c828ad0
Roger Espinosa <roger@umich.edu>
2023-03-02 11:22:07 -0600

    found the source code, whew

---
+++ imgsrv-tools/src/test/resources/org/hathitrust/stamper/update_config.js
@@ -0,0 +1,7 @@
+{
+    "input_filename": "src/test/resources/org/hathitrust/stamper/alice-in-wonderland.pdf",
+    "output_filename": "/tmp/update_full_output.pdf",
+    "stamper_filename": "src/test/resources/org/hathitrust/stamper/watermark.pdf",
+    "update_filepath": "/tmp/update_full",
+    "download_url": "/path/to/download/full_output.pdf"
+}
