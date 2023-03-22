4529b6e332be7182c2b3b97c6bb6145105d6f5aa
Aaron Elkiss <aelkiss@umich.edu>
2023-03-22 16:47:29 -0400

    correct name for load into solr; take args

--- slip/sample/load_into_solr.sh
+++ slip/sample/load_into_solr.sh
@@ -2,9 +2,7 @@
 
 SOLR_URL="http://localhost:8983/solr/core-x"
 echo "Indexing records into Solr..."
-nrec=$(/bin/ls *.xml | wc -l)
-i=0
-for file in $(/bin/ls *.xml);
+for file in "$@";
 do
   (echo '<add>'; cat $file; echo '</add>') |
    curl -s -X POST -H "Content-Type:application/xml"  --data-binary @- "$SOLR_URL/update"
