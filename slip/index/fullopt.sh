80a5a352080598a22333eaf34d47a981a54da560
Aaron Elkiss <aelkiss@umich.edu>
2024-06-25 12:34:53 -0400

    DEV-1219: Add a few useful things from production that were not in the repo

---
+++ slip/index/fullopt.sh
@@ -0,0 +1,14 @@
+./optimize-j -r11 -O1 -R1 &
+./optimize-j -r11 -O1 -R2 &
+./optimize-j -r11 -O1 -R3 &
+./optimize-j -r11 -O1 -R4 &
+./optimize-j -r11 -O1 -R5 &
+./optimize-j -r11 -O1 -R6 &
+./optimize-j -r11 -O1 -R7 &
+./optimize-j -r11 -O1 -R8 &
+./optimize-j -r11 -O1 -R9 &
+./optimize-j -r11 -O1 -R10 &
+./optimize-j -r11 -O1 -R11 &
+./optimize-j -r11 -O1 -R12 &
+
+
