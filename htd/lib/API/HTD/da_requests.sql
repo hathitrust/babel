f90851ea1ff2ccc842c04570d6f7e042805b3fdd
Aaron Elkiss <aelkiss@umich.edu>
2024-07-17 16:23:47 -0400

    DEV-1238: Remove htd, htdc, kgs
    See https://www.hathitrust.org/press-post/retirement-data-api/
    
    Also removes version of fetch_item that used the Data API (it didn't
    work anyway)

--- htd/lib/API/HTD/da_requests.sql
+++
@@ -1,6 +0,0 @@
--- ---------------------------------------------------------------------
--- HathiTrust Data API Client request data
--- ---------------------------------------------------------------------
-
-DROP TABLE IF EXISTS htd_requests;
-CREATE TABLE `htd_requests` (`access_key` varchar(64) NOT NULL default '', `nonce` varchar(64) NOT NULL default '', `stamptime` int NOT NULL default 0, KEY (`access_key`), KEY (`nonce`), KEY (`stamptime`));
