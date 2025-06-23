102bb585e03184180f70314b4cb7bd42b48c543d
Aaron Elkiss <aelkiss@umich.edu>
2025-06-23 09:29:35 -0400

    ETT-214: Multipage download for resource sharing (#117)
    * ETT-329: resource sharing user can download whole item
    * ETT-330: whole volume download test for resource sharing
    * Update tsv for resource sharing for multipage download
    - Modify RS tests based on anticipated go-ahead for google and page access profile full PDF downloads
    - Add tests and rights entries for RS + und-world with the appropriate access profiles (fixing db-image out of scope for the moment)
    * Playwright tests for not-held/not-currently-held items check for "This item cannot be downloaded" in the accordion.
    * Move `ssdproxy` PDF download logic close to RS user to fix "overpromising" in PT.
    - Enable PDF download tests for `ssdproxy` user in `mdp-lib/t/access_ssd_proxy_user.t`.
    
    ---------
    
    Co-authored-by: Brian Moses Hall <moseshll@umich.edu>

--- pt/web/firebird/tests/auth_resource_sharing_user.spec.js
+++ pt/web/firebird/tests/auth_resource_sharing_user.spec.js
@@ -18,6 +18,41 @@ test.describe('resource_sharing_user access to ic material', () => {
     await expect(page.getByRole('figure')).toBeVisible();
   });
 
+  test('download ic_currently_held whole item jpeg, full resolution', async ({ page }) => {
+    test.slow();
+
+    await page.goto('/cgi/pt?id=test.ic_currently_held');
+
+    const downloadAccordion = page.getByRole('heading', { name: 'Download' });
+    const downloadAccordionButton = downloadAccordion.getByRole('button', { name: 'Download' });
+    await downloadAccordionButton.click();
+    await page.getByRole('radio', { name: 'Image (JPEG)' }).check();
+    await page.getByLabel('Full / 600 dpi').check();
+    await page.getByLabel('Whole item').check();
+    await expect(page.getByRole('radio', { name: 'Image (JPEG)' })).toBeChecked();
+    await expect(page.getByLabel('Full / 600 dpi')).toBeChecked();
+    await expect(page.getByLabel('Whole item')).toBeChecked();
+    const downloadButton = page.getByRole('button', { name: 'Download', exact: true });
+    await downloadButton.click();
+
+    await expect(page.getByLabel('Building your Image (JPEG)')).toBeVisible();
+
+    await expect(page.getByRole('progressbar')).toBeHidden({ timeout: 9000 });
+    await expect(page.getByLabel('Building your Image (JPEG)').getByRole('paragraph')).toBeVisible({
+      timeout: 15_000,
+    });
+
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('link', { name: 'Download', exact: true }).click();
+    const download = await downloadPromise;
+    const downloadPath = await download.path();
+
+    //expect download to be zip
+    expect(download.suggestedFilename()).toContain('zip');
+    //expect file to exist before playwright deletes it
+    expect(fs.existsSync(downloadPath)).toBeTruthy();
+  });
+
 
   test('download single page from ic_currently_held', async ({ page }) => {
     await page.goto('/cgi/pt?id=test.ic_currently_held');
@@ -43,11 +78,21 @@ test.describe('resource_sharing_user access to ic material', () => {
   test('pageturner does not load image for ic_not_current', async ({ page }) => {
     await page.goto('/cgi/pt?id=test.ic_not_current');
     await expect(page.getByRole('figure')).toHaveCount(0);
+    // Download panel has "This item cannot be downloaded."
+    const downloadAccordion = page.getByRole('heading', { name: 'Download' });
+    const downloadAccordionButton = downloadAccordion.getByRole('button', { name: 'Download' });
+    await downloadAccordionButton.click();
+    await expect(page.getByText('This item cannot be downloaded')).toHaveCount(1);
   });
 
   test('pageturner does not load image for ic_not_held', async ({ page }) => {
     await page.goto('/cgi/pt?id=test.ic_not_held');
     await expect(page.getByText('not available online')).toHaveCount(1);
     await expect(page.getByRole('figure')).toHaveCount(0);
+    // Download panel has "This item cannot be downloaded."
+    const downloadAccordion = page.getByRole('heading', { name: 'Download' });
+    const downloadAccordionButton = downloadAccordion.getByRole('button', { name: 'Download' });
+    await downloadAccordionButton.click();
+    await expect(page.getByText('This item cannot be downloaded')).toHaveCount(1);
   });
 });
