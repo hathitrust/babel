fb5730095a8b08b009f89c8cd7a74cb7cec6dd7d
K'Ron Spar <kspar@umich.edu>
2025-06-24 11:40:50 -0400

    Pulled auth.sql changes from main for perl-test service.

--- sql/auth.sql
+++ sql/auth.sql
@@ -12,14 +12,11 @@ REPLACE INTO ht.ht_users (userid, displayname, email, usertype, role, access, ex
 ('rsuser@hathitrust.org','HathiTrust Resource-Sharinguser','rsuser@hathitrust.org','external','resource_sharing','normal',DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 5 YEAR),'^.*$','https://idp.hathitrust.org/entity','hathitrust');
 
 --- inserts/updates for ht_institutions
-REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations) 
+REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations)
   VALUES ('umich','University of Michigan','umich.edu','1','1','https://shibboleth.umich.edu/idp/shibboleth','^(member|alum|faculty|staff|student|employee)@umich.edu');
 
-REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations) 
+REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations)
   VALUES ('hathitrust','HathiTrust','hathitrust.org','1','1','https://idp.hathitrust.org/entity','^(member|alum|faculty|staff|student|employee)@hathitrust.org');
-
-REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations) 
-  VALUES ('nfb','National Federation of the Blind','nfb.org','1','1','pumex-idp','^(member)@nfb.org');
 
 REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, allowed_affiliations, emergency_status)  VALUES ('etas','ETAS Example Inst','etas.example','1','1','https://idp.etas.example','^(member)@etas.example','^(member)@etas.example');
 
@@ -27,3 +24,8 @@ REPLACE INTO ht.ht_institutions (inst_id, name, domain, us, enabled, entityID, a
 REPLACE INTO ht.rights_current (namespace, id, attr, reason, source, access_profile, user, note) values ('test','ic_currently_held','2','1','19','1','babel','Synthetic test item');
 REPLACE INTO ht.rights_current (namespace, id, attr, reason, source, access_profile, user, note) values ('test','ic_not_held','2','1','19','1','babel','Synthetic test item');
 REPLACE INTO ht.rights_current (namespace, id, attr, reason, source, access_profile, user, note) values ('test','ic_not_current','2','1','19','1','babel','Synthetic test item');
+
+--- sample data for unit testing und-world which has an impoverished set of entries in db-image
+REPLACE INTO ht.rights_current (namespace, id, attr, reason, source, access_profile, user, note) values ('test','und-world_open','18','1','19','1','babel','Synthetic test item');
+REPLACE INTO ht.rights_current (namespace, id, attr, reason, source, access_profile, user, note) values ('test','und-world_google','18','1','19','2','babel','Synthetic test item');
+REPLACE INTO ht.rights_current (namespace, id, attr, reason, source, access_profile, user, note) values ('test','und-world_page','18','1','19','3','babel','Synthetic test item');
