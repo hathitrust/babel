eaa87fd566d5adc18a97a48ad7bd7f5d3df44b33
Caryl Wyatt <carylw@hathitrust.org>
2025-12-08 14:03:49 -0500

    ETT-1131: add aria-hidden to icons

--- pt/web/firebird/src/js/components/AccessStatusPanel/InLibraryUser.svelte
+++ pt/web/firebird/src/js/components/AccessStatusPanel/InLibraryUser.svelte
@@ -39,7 +39,7 @@
 </script>
 
 <Panel {expanded} {onToggle} class="access-panel">
-  <i class="fa-solid fa-unlock" slot="icon"></i>
+  <i class="fa-solid fa-unlock" slot="icon" aria-hidden="true"></i>
   <slot:fragment slot="title">
     Checked out until <span class="expires-display">{expiresDisplay}</span>
   </slot:fragment>
