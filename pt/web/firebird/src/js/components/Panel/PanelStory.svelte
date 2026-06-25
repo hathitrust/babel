3aeb899495da9926c20a41de10e69662e6825c1f
eumalin <eumalin@users.noreply.github.com>
2026-06-25 14:26:37 -0400

    Add Storybook to pt/web/firebird

---
+++ pt/web/firebird/src/js/components/Panel/PanelStory.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Panel from './index.svelte';
+  let { expanded = false } = $props();
+</script>
+
+<Panel {expanded}>
+  <span slot="title">Example Panel</span>
+  <div slot="body"><p>Panel body content.</p></div>
+</Panel>
